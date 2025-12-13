eb4590dab319ae3277b5eac9c05ce35230112489
Sebastien Berton <sebasti1.berton@gmail.com>
2025-12-13 10:13:01 +0000

    Remove the timer from quests

--- Scripts/Condition.cs
+++ Scripts/Condition.cs
@@ -9,18 +9,7 @@ namespace Bakery
     public abstract class Condition : ScriptableObject
     {
         [SerializeField] private float _delay;
-        private string _timerId = string.Empty;
         public abstract bool Check { get; }
         public bool CanReverse = false;
-
-        public bool CheckTimer()
-        {
-            if (_timerId == string.Empty)
-            {
-                _timerId = Quests.Timers().StartNewTimer(_delay);
-                return false;
-            }
-            return !Quests.Timers().IsTimerRunning(_timerId);
-        }
     }
 }
--- Scripts/Quests.cs
+++ Scripts/Quests.cs
@@ -17,10 +17,10 @@ namespace Bakery
         }
 
         public static Func<IQuestManager> Manager = UnregisterManager;
-        public static Func<IQuestTimerManager> Timers = UnregisterTimer;
+
 
         private static IQuestManager _cachedManager = null;
-        private static IQuestTimerManager _cachedTimer = null;
+
 
         public static IQuestManager UnregisterManager()
         {
@@ -30,39 +30,6 @@ namespace Bakery
             return _cachedManager;
         }
 
-        public static IQuestTimerManager UnregisterTimer()
-        {
-            Debug.LogWarning("[QuestServices] No Quest Timer registered. Please register a Quest Timer before using Quest Services.");
-            _cachedTimer ??= new MockQuestTimer();
-            Timers = () => _cachedTimer;
-            return _cachedTimer;
-        }
-
-        private class MockQuestTimer : IQuestTimerManager
-        {
-            public void CancelTimer(string timerId)
-            { }
-
-            public float GetTimeLeft(string timerId)
-            { return 0f; }
-
-            public bool IsTimerRunning(string timerId)
-            {
-                return false;
-            }
-
-            public void PauseTimer(string timerId)
-            { }
-
-            public void RestartTimer(string timerId)
-            { }
-
-            public void ResumeTimer(string timerId)
-            { }
-
-            public string StartNewTimer(float duration, Action OnTimerEnd)
-            { return string.Empty; }
-        }
         private class MockQuestManager : IQuestManager
         {
             public WaitUntil WaitUntilReady => new(() => true);
